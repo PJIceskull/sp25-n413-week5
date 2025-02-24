a42475b99029d0438354eb73e10fd207a29d7ead
Pierce Issah <PJIceskull2003@gmail.com>
2025-02-24 16:59:55 -0500

    add lists for lessons & instructors

---
+++ wp-content/themes/collinsblock/build/instructors-list copy/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '133da937d9f7febaf716');
--- wp-content/themes/collinsblock/functions.php
+++ wp-content/themes/collinsblock/functions.php
@@ -3,5 +3,7 @@
 function collinsblocks_init_blocks()
 {
     register_block_type(__DIR__ . '/build/main-nav');
+    register_block_type(__DIR__ . '/build/instructors-list');
+    register_block_type(__DIR__ . '/build/lessons-list');
 }
 add_action('init', 'collinsblocks_init_blocks');
